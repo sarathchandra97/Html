4ff73104b766de6ad6bd7ff6e3f24f0085121302
sarath <sarath.yakkati@gmail.com>
2019-09-29 09:02:14 +0530

    Moved Angular and React to JS repo

--- angular/todo-sample.js
+++
@@ -1,8 +0,0 @@
-angular.module("tsm", [])
-.controller("tsc" , function(){
-    var tsv = this;
-    tsv.names=['sarath','chandra'];
-    tsv.add= function(){
-        tsv.names.push(tsv.tobeadded);
-    }
-});
